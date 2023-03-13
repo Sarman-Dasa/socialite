da76f6fcfd3f9dad5814a44f6ff4ef89cff24472
Sarman-Dasa <sarmand@zignuts.com>
2023-03-13 15:12:07 +0530

    add colunm in user table

---
+++ database/migrations/2023_03_13_091334_add_column_from_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('provider_id')->nullable();
+            $table->string('provider_name',100)->nullable();
+            $table->string('avatar',200)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('provider_id');
+            $table->dropColumn('provider_name');
+            $table->dropColumn('avatar');
+        });
+    }
+};
