839bf96138b3c7d8bef42b90c8ee8996f26182a5
Sarman-Dasa <sarmand@zignuts.com>
2023-03-13 19:33:31 +0530

    login with github and google using socialite

--- app/Http/Controllers/LoginWithSocialiteController.php
+++ app/Http/Controllers/LoginWithSocialiteController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Exception;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,14 +12,19 @@ use Illuminate\Support\Str;
 
 class LoginWithSocialiteController extends Controller
 {
+
+
+    //github login
     public function githubRedirect()
     {
         return Socialite::driver('github')->redirect();
+
     }
 
     public function githubCallback()
     {
         $gitUser = Socialite::driver('github')->user();
+
         $user = User::updateOrCreate(
             ['email' => $gitUser->email],
             [
@@ -29,7 +35,45 @@ class LoginWithSocialiteController extends Controller
                 'password'      => Hash::make(12345678),
             ]
         );
+
         Auth::login($user);
         return redirect('/dashboard');
+    }
+
+    //Google login
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function GoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->user();
+        $user = User::updateOrCreate(
+            ['email' => $googleUser->email],
+            [
+                'name'          => $googleUser->name,
+                'provider_name'      => 'google',
+                'provider_id'   => $googleUser->id,
+                'avatar'        => $googleUser->avatar,
+                'password'      => Hash::make(12345678),
+            ]
+        );
+        Auth::login($user);
+        return redirect('/dashboard');
+    }
+
+    //linkedin login
+
+    public function redirectTolinkedin()
+    {
+      
+        return Socialite::driver('linkedin')->stateless()->scopes(['r_liteprofile', 'r_emailaddress'])->redirect();
+    }
+
+    public function linkedinCallback()
+    {
+        $linkedUser = Socialite::driver('linkedin')->user();
+        dd($linkedUser);
     }
 }
